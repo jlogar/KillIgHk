e2d9374b2ead4e6f9db3977ab5f13332de4a3726
Jernej Logar <jernej.logar@gmail.com>
2015-11-23 19:37:27 +0100

    added retry loop for waiting at startup (can't really expect the process to be the first thing that loads)

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Diagnostics;
+using System.Threading;
 
 namespace KillIgHk
 {
@@ -10,9 +11,23 @@ namespace KillIgHk
         {
             Console.WriteLine("Looking for igfxHK processes...");
             //as seen @ http://stackoverflow.com/a/116098/120427
-            var processes = Process.GetProcessesByName("igfxHK");
+            var retry = 0;
+            var processes = new Process[0];
+            while (retry < 5)
+            {
+                processes = Process.GetProcessesByName("igfxHK");
+                if (processes.Length == 0)
+                {
+                    retry++;
+                    Console.WriteLine("Couldn't find any. Waiting a couple seconds and retrying (retry nr. {0}).", retry);
+                    Thread.Sleep(2000);
+                    continue;
+                }
+                break;
+            }
             if (processes.Length == 0)
-                Console.WriteLine("Couldn't find any. Exiting.");
+                Console.WriteLine("Couldn't find any. Nothing done.");
+
             foreach (var p in processes)
             {
                 Console.WriteLine("Got a igfxHK process. Trying to kill it.");
@@ -27,13 +42,13 @@ namespace KillIgHk
                 {
                     Console.ForegroundColor = ConsoleColor.Yellow;
                     //just loggin since I just want to try (!) killing it
-                    Console.WriteLine("Error killing igfxHK.", ex);
+                    Console.WriteLine("Error killing igfxHK.\n{0}", ex);
                 }
                 catch (InvalidOperationException ex)
                 {
                     Console.ForegroundColor = ConsoleColor.Yellow;
                     //just loggin since I just want to try (!) killing it
-                    Console.WriteLine("Error killing igfxHK.", ex);
+                    Console.WriteLine("Error killing igfxHK.\n{0}", ex);
                 }
             }
         }
